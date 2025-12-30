f7020edc110d75fa4b601397cd2c7dd13d6cfb6e
unknown <lwsnmensah@gmail.com>
2025-12-30 13:44:02 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Mobile menu toggle
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
+
+// Form validation on contact page (included in contact.html)
